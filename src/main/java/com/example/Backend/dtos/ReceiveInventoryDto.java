881381d96c65f0da15a570f595dc0c7a87b9fb20
erikcerne <erik.cerne2@gmail.com>
2026-04-22 23:16:42 +0200

    creted add to inventory dto

---
+++ src/main/java/com/example/Backend/dtos/ReceiveInventoryDto.java
@@ -0,0 +1,7 @@
+package com.example.Backend.dtos;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ReceiveInventoryDto(LocalDateTime expiryDate, LocalDateTime produceDate, int Quantity, UUID InventoryId, UUID UserID) {
+}
