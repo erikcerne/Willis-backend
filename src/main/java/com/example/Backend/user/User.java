ccbccdaf1cc36b9e83e20a1aba623038261a168c
erikcerne <erik.cerne2@gmail.com>
2026-04-23 15:33:36 +0200

    bild the user table

--- src/main/java/com/example/Backend/user/User.java
+++ src/main/java/com/example/Backend/user/User.java
@@ -1,8 +1,10 @@
 package com.example.Backend.user;
 
+import com.example.Backend.inventory.Inventory;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -13,4 +15,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.UUID)
     UUID UserID;
     String Name;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Inventory> inventories;
+
+    public User(){
+    }
+
+    public User(UUID userID, String name, List<Inventory> inventories) {
+        UserID = userID;
+        Name = name;
+        this.inventories = inventories;
+    }
 }
